bd1504ab290df70c7d55561590d0f41a998bec12
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-05-21 09:20:09 +0200

    update about txt

--- minishop/src/pages/About/About.jsx
+++ minishop/src/pages/About/About.jsx
@@ -6,8 +6,8 @@ export default function About() {
       <h2>Om MiniShop</h2>
       <p>
         MiniShop er en moderne og alsidig butik. Her finder du et bredt udvalg
-        af varer – lige fra dagligvarer og tøj til elektronik, boligartikler og
-        legetøj – alt sammen under ét tag. MiniShop fokuserer på gode priser,
+        af varer - lige fra dagligvarer og tøj til elektronik, boligartikler og
+        legetøj - alt sammen under ét tag. MiniShop fokuserer på gode priser,
         høj kvalitet og en nem indkøbsoplevelse for hele familien. Det er det
         ideelle sted til både hverdagens behov og større indkøb.
       </p>
