05f2b501779147646e1e7c154b34ae5923a227bc
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-05-21 09:45:47 +0200

    about and form styling

--- minishop/src/pages/About/About.jsx
+++ minishop/src/pages/About/About.jsx
@@ -1,8 +1,9 @@
 import FormWithFocus from "../../components/FormWithFocus/FormWithFocus";
+import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <div>
+    <div className={styles.about}>
       <h2>Om MiniShop</h2>
       <p>
         MiniShop er en moderne og alsidig butik. Her finder du et bredt udvalg
@@ -11,9 +12,13 @@ export default function About() {
         høj kvalitet og en nem indkøbsoplevelse for hele familien. Det er det
         ideelle sted til både hverdagens behov og større indkøb.
       </p>
-      <p>Kontakt os for mere information.</p>
+      <div className={styles.contact}>
+        <p>Kontakt os for mere information</p>
+      </div>
       <FormWithFocus />
-      <p>Vi værdsætter dit feedback og dine henvendelser.</p>
+      <p>
+        <em>Vi værdsætter dit feedback og dine henvendelser.</em>
+      </p>
     </div>
   );
 }
