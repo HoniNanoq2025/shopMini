60dde4568173ee8f2cc156d97be6e11c8c63f71e
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-05-21 13:48:51 +0200

    update Home

--- minishop/src/pages/Home/Home.jsx
+++ minishop/src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import TopRatedProductSlider from "../../components/TopRatedProductSlider/TopRatedProductSlider";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <h1>Velkommen til MiniShop</h1>
       <p>We are ready to sell you stuff that you need. And also stuff that you really don't need.</p>
       <div>
